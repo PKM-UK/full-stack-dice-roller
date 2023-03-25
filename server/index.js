a29b83fc2a45617031b0702af1e130bce2412f3b
Martin de Selincourt <pyrophernalia@gmail.com>
2023-03-25 17:27:00 +0000

    DnD 5e, Dungeon World and BitD skills supported

--- server/index.js
+++ server/index.js
@@ -9,8 +9,10 @@ const app = express();
 app.get("/api", (req, res) => {
   // res.json({message: "Strength +4"})
   res.json([
-    {desc: "Strength +3", modi: 3},
-    {desc: "Dex +1", modi: 1}
+    {type: "5e", desc: "Strength +3", modi: 3},
+    {type: "5e", desc: "Dex +1", modi: 1},
+    {type: "dw", desc: "Spout Lore +2", modi: 2},
+    {type: "bitd", desc: "Wreck 3d", modi: 3}
   ]);
 });
 
