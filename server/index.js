2a5d2ed4abf14d7d8c15f8654de870d83a2ca2ff
Martin de Selincourt <pyrophernalia@gmail.com>
2023-03-26 23:09:56 +0100

    App now loading some skills from mongodb

--- server/index.js
+++ server/index.js
@@ -1,19 +1,39 @@
-console.log('Derp')
-
 const express = require("express");
+const { MongoClient } = require('mongodb');
+
+// Connection bits
+const url = 'mongodb://127.0.0.1:27017';
+const client = new MongoClient(url);
+const dbName = 'test';
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.get("/api", (req, res) => {
+async function getMongoStuff() {
+  // Use connect method to connect to the server
+  await client.connect();
+  const db = client.db(dbName);
+  const collection = db.collection('skills');
+
+  const findResult = await collection.find({"charid": 1}).toArray();
+  console.log('Found documents =>', findResult);
+
+  return findResult;
+}
+
+app.get("/api", async (req, res) => {
   // res.json({message: "Strength +4"})
-  res.json([
-    {type: "5e", desc: "Strength +3", modi: 3},
-    {type: "5e", desc: "Dex +1", modi: 1},
-    {type: "dw", desc: "Spout Lore +2", modi: 2},
-    {type: "bitd", desc: "Wreck 3d", modi: 3}
-  ]);
+  mongojson = await getMongoStuff();
+
+  /* res.json([
+    {type: "dnd", desc: "Strength +3", mod: 3},
+    {type: "dnd", desc: "Dex +1", mod: 1},
+    {type: "dw", desc: "Spout Lore +2", mod: 2},
+    {type: "bitd", desc: "Wreck 3d", mod: 3}
+  ]); */
+
+  res.json(mongojson);
 });
 
 app.listen(PORT, () => {
